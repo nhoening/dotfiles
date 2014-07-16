b4d8d4f01216c282e84e341f5a90bc1e03999b1b
nhoening <iam@nicolashoening.de>
2014-07-16 14:53:23 +0200

    correct handling of existing paths/git-dirs; do absolutely nothing if things exist

--- install.sh
+++ install.sh
@@ -4,40 +4,49 @@ cd ~
 
 DOIT=1
 
-# TODO: paths are not checked this way
-for D in .bashrc .vimrc .gitconfig .hgrc .ssh/config .vim/bundle/Vundle.vim liquidprompt 
+# check if files exist
+for F in .bashrc .vimrc .gitconfig .hgrc .ssh/config  
 do
-    echo "checking $D ..."
-    if [ -f "$D" ]; then
-        if [ "$1" == "-F" ]; then
-            printf '%s\n' "Removing File/Folder: ($D)"
-            rm -rf "$D"
+    echo "checking $F ..."
+    if [ -f "$F" ]; then
+        if [ "$1" == "-f" ]; then
+            printf '%s\n' "Removing File/Folder: ($F)"
+            rm -rf $D
         else
-            printf '%s\n' "Existing: ($D). To force overwrite, call with -F option"
+            printf '%s\n' "Existing: ($F). I will not do anything. To force overwrite, call with -f option"
             DOIT=0
         fi
     fi
 done
 
 if [ $DOIT -eq 1 ]; then
-    ln -s dotfiles/.bashrc
-    ln -s dotfiles/.vimrc
-    ln -s dotfiles/.gitconfig
-    ln -s dotfiles/.hgrc
-    ln -s dotfiles/.sshconfig .ssh/config
+    ln -fs dotfiles/.bashrc
+    ln -fs dotfiles/.vimrc
+    ln -fs dotfiles/.gitconfig
+    ln -fs dotfiles/.hgrc
+    mkdir -p ~/.ssh
+    ln -fs ~/dotfiles/.sshconfig ~/.ssh/config
 
-    git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
+    if [ -d ".vim/bundle/Vundle.vim" ]; then
+        cd .vim/bundle/Vundle.vim; git pull; cd ../../..
+    else
+        git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    fi
 
-    git clone https://github.com/nojhan/liquidprompt.git
-    source liquidprompt/liquidprompt
+    if [ -d "liquidprompt" ]; then
+        cd liquidprompt; git pull; cd ..
+    else
+        git clone https://github.com/nojhan/liquidprompt.git
+    fi
+
+    # install vim bundles (vundle Plugins)
+    vim +PluginInstall +qall
+
+    # install YouCompleteMe
+    # Non-debian: To set up your vim environment, run the command :PluginInstall after launching vim.
+    #sudo apt-get install build-essential cmake python-dev
+    #cd ~/.vim/bundle/YouCompleteMe
+    #./install.sh
+    cd ~
+
 fi
-
-# install vim bundles (vundle Plugins)
-vim +PluginInstall +qall
-
-# install YouCompleteMe
-# Non-ebian: To set up your vim environment, run the command :PluginInstall after launching vim.
-sudo apt-get install build-essential cmake python-dev
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh
-
